b89985f6c8d09e1a82d7db512c701295c231ed7a
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2016-04-09 13:00:43 +0200

    Eliminados structs innecesarios en la TF

--- 3DCurator/transferFunction.cpp
+++ 3DCurator/transferFunction.cpp
@@ -4,115 +4,85 @@ TransferFunction::TransferFunction() {
 	colorFun = vtkSmartPointer<vtkColorTransferFunction>::New();
 	scalarFun = vtkSmartPointer<vtkPiecewiseFunction>::New();
 	gradientFun = vtkSmartPointer<vtkPiecewiseFunction>::New();
-	tf.name = "tf";
-	tf.description = "";
+	name = "tf";
+	description = "";
 }
 
 void TransferFunction::read(std::string &filename) {
+	clear();
+
 	using boost::property_tree::ptree;
 	ptree pt;
-
 	read_xml(filename, pt);
 
-	TF tf;
-	tf.name = pt.get<std::string>("tf.<xmlattr>.name");
-	tf.description = pt.get<std::string>("tf.<xmlattr>.description");
+	name = pt.get<std::string>("tf.<xmlattr>.name");
+	description = pt.get<std::string>("tf.<xmlattr>.description");
 
 	BOOST_FOREACH(ptree::value_type const &v, pt.get_child("tf.color")) {
 		if (v.first == "point") {
-			ColorPoint cp;
-			cp.x = v.second.get<double>("x");
-			cp.r = v.second.get<double>("r");
-			cp.g = v.second.get<double>("g");
-			cp.b = v.second.get<double>("b");
-			cp.m = v.second.get<double>("m", 0.5);
-			cp.s = v.second.get<double>("s", 0.0);
-			tf.colorTF.push_back(cp);
+			colorFun->AddRGBPoint(
+				v.second.get<double>("x"),
+				v.second.get<double>("r"),
+				v.second.get<double>("g"),
+				v.second.get<double>("b")
+			);
 		}
 	}
 
 	BOOST_FOREACH(ptree::value_type const &v, pt.get_child("tf.scalar")) {
 		if (v.first == "point") {
-			OpacityPoint op;
-			op.x = v.second.get<double>("x");
-			op.y = v.second.get<double>("y");
-			op.m = v.second.get<double>("m", 0.5);
-			op.s = v.second.get<double>("s", 0.0);
-			tf.scalarTF.push_back(op);
+			scalarFun->AddPoint(
+				v.second.get<double>("x"),
+				v.second.get<double>("y")
+			);
 		}
 	}
 
 	BOOST_FOREACH(ptree::value_type const &v, pt.get_child("tf.gradient")) {
 		if (v.first == "point") {
-			OpacityPoint op;
-			op.x = v.second.get<double>("x");
-			op.y = v.second.get<double>("y");
-			op.m = v.second.get<double>("m", 0.5);
-			op.s = v.second.get<double>("s", 0.0);
-			tf.gradientTF.push_back(op);
+			gradientFun->AddPoint(
+				v.second.get<double>("x"),
+				v.second.get<double>("y")
+				);
 		}
 	}
-
-	this->tf = tf;
-
-	update();
 }
 
 void TransferFunction::write(std::string &filename) {
 	using boost::property_tree::ptree;
 	ptree pt;
 
-	cout << tf.colorTF.size() << endl;
+	double *colorFunData = colorFun->GetDataPointer();
+	double *scalarFunData = scalarFun->GetDataPointer();
+	double *gradientFunData = gradientFun->GetDataPointer();
 
-	pt.put("tf.<xmlattr>.name", tf.name);
-	pt.put("tf.<xmlattr>.description", tf.description);
+	pt.put("tf.<xmlattr>.name", name);
+	pt.put("tf.<xmlattr>.description", description);
 
 	ptree &color = pt.add("tf.color", "");
-	BOOST_FOREACH(ColorPoint cp, tf.colorTF) {
+	for (int i = 0; i < colorFun->GetSize(); i++) {
 		ptree &node = color.add("point", "");
-		node.put("x", cp.x);
-		node.put("r", cp.r);
-		node.put("g", cp.g);
-		node.put("b", cp.b);
-		node.put("m", cp.m);
-		node.put("s", cp.s);
+		node.put("x", colorFunData[i * 4]);
+		node.put("r", colorFunData[i * 4 + 1]);
+		node.put("g", colorFunData[i * 4 + 2]);
+		node.put("b", colorFunData[i * 4 + 3]);
 	}
 
 	ptree &scalar = pt.add("tf.scalar", "");
-	BOOST_FOREACH(OpacityPoint op, tf.scalarTF) {
+	for (int i = 0; i < scalarFun->GetSize(); i++) {
 		ptree &node = scalar.add("point", "");
-		node.put("x", op.x);
-		node.put("y", op.y);
-		node.put("m", op.m);
-		node.put("s", op.s);
+		node.put("x", scalarFunData[i * 2]);
+		node.put("y", scalarFunData[i * 2 + 1]);
 	}
 
 	ptree &gradient = pt.add("tf.gradient", "");
-	BOOST_FOREACH(OpacityPoint op, tf.gradientTF) {
-		ptree &node = gradient.add("point", "");
-		node.put("x", op.x);
-		node.put("y", op.y);
-		node.put("m", op.m);
-		node.put("s", op.s);
+	for (int i = 0; i < gradientFun->GetSize(); i++) {
+		ptree &node = scalar.add("point", "");
+		node.put("x", gradientFunData[i * 2]);
+		node.put("y", gradientFunData[i * 2 + 1]);
 	}
 
 	write_xml(filename, pt);
-}
-
-void TransferFunction::update() {
-	clear();
-
-	BOOST_FOREACH(ColorPoint p, tf.colorTF) {
-		colorFun->AddRGBPoint(p.x, p.r, p.g, p.b, p.m, p.s);
-	}
-
-	BOOST_FOREACH(OpacityPoint p, tf.scalarTF) {
-		scalarFun->AddPoint(p.x, p.y, p.m, p.s);
-	}
-
-	BOOST_FOREACH(OpacityPoint p, tf.gradientTF) {
-		gradientFun->AddPoint(p.x, p.y, p.m, p.s);
-	}
 }
 
 vtkSmartPointer<vtkColorTransferFunction> TransferFunction::getColorFun() const {
@@ -134,65 +104,13 @@ void TransferFunction::clear() {
 }
 
 void TransferFunction::addColorPoint(const double x, const double r, const double g, const double b) {
-	ColorPoint p;
-	p.x = x;
-	p.r = r;
-	p.g = g;
-	p.b = b;
-	p.m = 0.5;
-	p.s = 0.0;
-	tf.colorTF.push_back(p);
-	colorFun->AddRGBPoint(p.x, p.r, p.g, p.b, p.m, p.s);
-}
-
-void TransferFunction::addColorPoint(const double x, const double r, const double g, const double b, const double m, const double s) {
-	ColorPoint p;
-	p.x = x;
-	p.r = r;
-	p.g = g;
-	p.b = b;
-	p.m = m;
-	p.s = s;
-	tf.colorTF.push_back(p);
-	colorFun->AddRGBPoint(p.x, p.r, p.g, p.b, p.m, p.s);
+	colorFun->AddRGBPoint(x, r, g, b);
 }
 
 void TransferFunction::addScalarPoint(const double x, const double y) {
-	OpacityPoint p;
-	p.x = x;
-	p.y = y;
-	p.m = 0.5;
-	p.s = 0.0;
-	tf.scalarTF.push_back(p);
-	scalarFun->AddPoint(p.x, p.y, p.m, p.s);
-}
-
-void TransferFunction::addScalarPoint(const double x, const double y, const double m, const double s) {
-	OpacityPoint p;
-	p.x = x;
-	p.y = y;
-	p.m = m;
-	p.s = s;
-	tf.scalarTF.push_back(p);
-	scalarFun->AddPoint(p.x, p.y, p.m, p.s);
+	scalarFun->AddPoint(x, y);
 }
 
 void TransferFunction::addGradientPoint(const double x, const double y) {
-	OpacityPoint p;
-	p.x = x;
-	p.y = y;
-	p.m = 0.5;
-	p.s = 0.0;
-	tf.gradientTF.push_back(p);
-	gradientFun->AddPoint(p.x, p.y, p.m, p.s);
-}
-
-void TransferFunction::addGradientPoint(const double x, const double y, const double m, const double s) {
-	OpacityPoint p;
-	p.x = x;
-	p.y = y;
-	p.m = m;
-	p.s = s;
-	tf.gradientTF.push_back(p);
-	gradientFun->AddPoint(p.x, p.y, p.m, p.s);
+	gradientFun->AddPoint(x, y);
 }
--- 3DCurator/transferFunction.h
+++ 3DCurator/transferFunction.h
@@ -13,34 +13,6 @@
 #include "vtkColorTransferFunction.h"
 #include "vtkPiecewiseFunction.h"
 
-struct ColorPoint {
-	double x;
-	double r;
-	double g;
-	double b;
-	double m;
-	double s;
-};
-
-struct OpacityPoint {
-	double x;
-	double y;
-	double m;
-	double s;
-};
-
-typedef std::vector<ColorPoint> ColorTF;
-typedef std::vector<OpacityPoint> ScalarTF;
-typedef std::vector<OpacityPoint> GradientTF;
-
-struct TF {
-	ColorTF colorTF;
-	ScalarTF scalarTF;
-	GradientTF gradientTF;
-	std::string name;
-	std::string description;
-};
-
 class TransferFunction {
 
 public:
@@ -58,15 +30,13 @@ public:
 	void update();
 	void clear();
 	void addColorPoint(const double x, const double r, const double g, const double b);
-	void addColorPoint(const double x, const double r, const double g, const double b, const double m, const double s);
 	void addScalarPoint(const double x, const double y);
-	void addScalarPoint(const double x, const double y, const double m, const double s);
 	void addGradientPoint(const double x, const double y);
-	void addGradientPoint(const double x, const double y, const double m, const double s);
 
 private:
 	// atributos
-	struct TF tf;
+	std::string name;
+	std::string description;
 	vtkSmartPointer<vtkColorTransferFunction> colorFun;
 	vtkSmartPointer<vtkPiecewiseFunction> scalarFun;
 	vtkSmartPointer<vtkPiecewiseFunction> gradientFun;

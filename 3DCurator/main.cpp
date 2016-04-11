0a63307516d52ef916bb67cf20f2ed3a3501187e
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2016-04-11 15:46:21 +0200

    Internacionalización

--- 3DCurator/main.cpp
+++ 3DCurator/main.cpp
@@ -1,8 +1,16 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QTranslator>
+#include <QLibraryInfo>
 
 int main(int argc, char *argv[]) {
-    QApplication a(argc, argv);
+	QTranslator qtTranslator;
+	qtTranslator.load("qt_" + QLocale::system().name(), QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+    
+	QApplication a(argc, argv);
+
+	a.installTranslator(&qtTranslator);
+
     MainWindow w;
     w.show();
 
